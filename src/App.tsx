b405ed82d0d15351bc014a7b91d347278d4d73fb
Bruno <brunodossantosportela@gmail.com>
2024-09-29 17:34:09 -0300

    Tarefa finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://scontent.fpoa14-1.fna.fbcdn.net/v/t31.18172-8/22770947_1876016012425225_4620003657850310043_o.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0327a3&_nc_ohc=xyZ5M6RUE-gQ7kNvgHxoxVh&_nc_ht=scontent.fpoa14-1.fna&_nc_gid=A7HKfEELRWKLZEPqXnLWtHh&oh=00_AYCRSU2f6gcGixi3XJABaZOQtIAqRK3kCs8Iuh0O00e-1A&oe=67212B53">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,35 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Comentários devem renderizar', async () => {
+        const { debug } = render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+            target: {
+                value: 'Carambolas... muito loko'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        await setTimeout(() => {
+            fireEvent.change(screen.getByTestId('campo-tarefa'), {
+                target: {
+                    value: 'Concordo com o comentário acima'
+                }
+            })
+            fireEvent.click(screen.getByTestId('btn-comentar'))
+
+            expect(screen.getByText('Carambolas... muito loko')).toBeInTheDocument()
+        }, 600)
+        await waitFor(() => {
+            expect(screen.getByText('Concordo com o comentário acima')).toBeInTheDocument()
+        })
+        debug()
+    })
 });
